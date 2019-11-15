3ee3e1b3601bbe19394249499ba923d3d59dc8a0
patriciakasapa <patriciakasapa07@gmail.com>
2019-11-15 08:51:54 +0000

    Done

---
+++ src/turntabl/first/IgnorantClient.java
@@ -0,0 +1,11 @@
+package turntabl.first;
+
+public class IgnorantClient {
+    /* Client */
+
+        public static void main(String[] args) {
+            SoupMakerFacade lightSoup = new SoupMakerFacade();
+            lightSoup.cook();
+        }
+    }
+
